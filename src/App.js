0b7ed9dd3c8bb1cde7c991a94c80cc002dbb0c35
musaab-khan <musaabkhan4511@gmail.com>
2023-12-30 12:18:52 +0500

    textUtils
    finalized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter,
+  // Switch,
+  Route,
+  // Link,
+  Routes
+} from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
+
+  //custom color theme has some un-resolved bugs but i'm leaving it for the future
+  //bugs include about us section changing, buttons color and navbar keeps toggling b/w light and dark
+  
+  // const removeClasses = () =>{
+  //   document.body.classList.remove('bg-light');
+  //   document.body.classList.remove('bg-dark');
+  //   document.body.classList.remove('bg-success');
+  //   document.body.classList.remove('bg-primary');
+  //   document.body.classList.remove('bg-warning');
+  //   document.body.classList.remove('bg-danger');
+  // }
+
+  const toggleMode = (cls)=>{
+
+    // console.log(cls)
+    // removeClasses();
+    // document.body.classList.add('bg-'+cls)
+    if(mode === 'light'){
+      setMode('dark');
+      // document.body.style.backgroundColor = '#042743';
+      document.body.style.backgroundColor = 'black';
+      document.body.style.color = 'white';
+      showAlert("Dark mode has been enabled", "success");
+      document.title = 'TextUtils - Dark Mode';
+      
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      document.title = 'TextUtils - Light Mode';
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+    {/* <Navbar/> */}
+     <BrowserRouter>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <Routes>
+    {/* /users --> Component 1
+        /users/home --> Component 2 */}
+          <Route exact path="/about" element={<About mode={mode}/>}></Route>
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter Text to analyze " mode={mode} /> } ></Route>
+    </Routes>
     </div>
+    </BrowserRouter>
+    </> 
   );
 }
 
